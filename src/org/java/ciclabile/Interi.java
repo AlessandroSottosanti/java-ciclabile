3d903f88f2f19d4b007f457f0a7c4f6202634937
AlessandroSottosanti <asottosanti2@gmail.com>
2025-03-27 17:09:38 +0100

    feat: Bonus

--- src/org/java/ciclabile/Interi.java
+++ src/org/java/ciclabile/Interi.java
@@ -9,6 +9,23 @@ public class Interi {
         this.numArray = array;
     }
 
+    public Interi() {
+        this.numArray = new int[0];
+    }
+
+    public void addElemento(int num) {
+        
+        int[] newArray = new int[numArray.length + 1];
+
+        for(int i = 0; i < numArray.length; i++) {
+            newArray[i] = numArray[i];
+        }
+
+        newArray[newArray.length - 1] = num;
+
+        numArray = newArray;
+    }
+
     public boolean hasAncoraElementi() {
         return counter < numArray.length;
     }
--- src/org/java/ciclabile/Main.java
+++ src/org/java/ciclabile/Main.java
@@ -3,23 +3,68 @@ package org.java.ciclabile;
 public class Main {
     public static void main(String[] args) {
 
-        final int[] numArray = {1, 2, 3, 4, 5};
+        final int[] numArray = { 1, 2, 3, 4, 5 };
+
+        // Costruttore con array di int atteso
+
+        System.out.println("Costruttore con array di interi atteso");
 
         Interi obj = new Interi(numArray);
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
-            obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
+        obj.getElementoSuccessivo();
 
+
+        System.out.println();
+        System.out.println();
+        
+        System.out.println("Costruttore senza parametri attesi");
+
+        
+        // Costruttore senza parametri attesi
+
+        Interi obj2 = new Interi();
+        obj2.addElemento(1);
+        obj2.addElemento(2);
+        obj2.addElemento(3);
+        obj2.addElemento(4);
+        obj2.addElemento(5);
+        obj2.addElemento(6);
+        obj2.addElemento(7);
+
+
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+
+        // in questo caso se prima di aggiungere i nuovi elementi cerco quello successivo all'ultimo presente, non lo trovo e ricomincia dacapo.
+        // se invece aggiungo altri elementi l'array si allunga. Commentare e decomentare il codice sotto per testarlo:
+
+        // obj2.getElementoSuccessivo();
+
+        obj2.addElemento(8);
+        obj2.addElemento(9);
+        obj2.addElemento(10);
+        obj2.addElemento(11);
+        obj2.addElemento(12);
+        obj2.addElemento(13);
+        obj2.addElemento(14);
+
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
+        obj2.getElementoSuccessivo();
     }
 }
